243f0b76580997c2a4211365d5c842fe36baee38
jaswant-kushwaha <jaswant@tensorblue.com>
2024-06-21 15:14:38 +0530

    middel ware added

---
+++ Middlewares/index.ts
@@ -0,0 +1,38 @@
+import { Request, Response, NextFunction } from 'express';
+import jwt, { TokenExpiredError } from 'jsonwebtoken';
+import User from '../Models/User'; // Adjust the path based on your project structure
+
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const authorizationHeaderToken = req.headers.authorization;
+        if (!authorizationHeaderToken) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+
+        const token = authorizationHeaderToken;
+        const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as { email: string };
+
+        const user = await User.findOne({ email: decoded.email }).select("-password");
+        if (!user) {
+            return res.status(401).json({
+                message: "Unauthorized"
+            });
+        }
+
+        (req as any).email = decoded.email;
+        next();
+    } catch (error) {
+        if (error instanceof TokenExpiredError) {
+            return res.status(401).json({
+                message: "Token expired"
+            });
+        }
+
+        console.error(typeof error, error);
+        res.status(500).json({
+            message: "Something went wrong"
+        });
+    }
+};
